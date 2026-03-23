fc399f0130b043191d656b3a04450de8f74d93af
Roman Zharov <rzharov.dev@gmail.com>
2026-03-23 14:05:51 +0300

    files comparing functionality

--- src/Runner.php
+++ src/Runner.php
@@ -3,12 +3,12 @@
 namespace GenDiff\Runner;
 
 use function GenDiff\Core\readFile;
+use function GenDiff\Builder\build;
 
-function run(string $path1, string $path2, $format)
+function run(string $path1, string $path2, $format): array
 {
-    $data1 = readFile($path1);
-    $data2 = readFile($path2);
+    $data1 = get_object_vars(json_decode(readFile($path1)));
+    $data2 = get_object_vars(json_decode(readFile($path2)));
 
-    var_dump($data1);
-    var_dump($data2);
+    return build($data1, $data2);
 }
